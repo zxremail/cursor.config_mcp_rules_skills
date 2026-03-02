2611c0f2106aaf1301bdf55845d6bfef7a9044b8
zhuxingrui <zhuxingrui@rigol.com>
2026-03-02 19:43:07 +0800

    ooo000ooo
    Change-Id: Id82f6f19bee9585c23d15b01651a6ab9f85bad56

--- x_config.sh
+++ x_config.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-cp -a .gitignore.more_for_cursor         projects/.gitignore
-cp -a .gitignore.more_for_cursor         skills-cursor/.gitignore
+if [[ -d projects/ ]]; then
+    cp -a .gitignore.more_for_cursor         projects/.gitignore
+fi
+
+if [[ -d skills-cursor/ ]]; then
+    cp -a .gitignore.more_for_cursor         skills-cursor/.gitignore
+fi
 
