8be8ee521debda116e5fd0da6a82bdbb0dfac996
adil.zhu <zxremail@gmail.com>
2026-04-14 21:07:09 +0800

    ooo000ooo
    Change-Id: I3459ebfd3b116bd32ee72af72b415a6114949994

--- x_config.sh
+++ x_config.sh
@@ -3,6 +3,7 @@
 mkdir projects
 mkdir skills-cursor
 mkdir plans
+mkdir plugins
 
 if [[ -d projects/ ]]; then
     cp -a .gitignore.more_for_cursor         projects/.gitignore
@@ -10,6 +11,10 @@ fi
 
 if [[ -d skills-cursor/ ]]; then
     cp -a .gitignore.more_for_cursor         skills-cursor/.gitignore
+fi
+
+if [[ -d plugins/ ]]; then
+    cp -a .gitignore.more_for_cursor         plugins/.gitignore
 fi
 
 if [[ -d plans/ ]]; then
